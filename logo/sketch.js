42a35c5dbb1dabd19b8cb61fc697ce1a82113483
JimmyTheDon <jdon@cca.edu>
2017-03-26 17:33:06 -0700

    Another try
    Let go!!

--- logo/sketch.js
+++ logo/sketch.js
@@ -159,3 +159,166 @@ var y = "Y";
   }
 }
 var myp5 = new p5(side1);
+
+
+var side2 = function(side2) {
+var font;
+var letterj, letteri, letterm1, letterm2, lettery;
+var xpos, ypos, xspeed, yspeed;
+var w = 300;
+var h = 300;
+var j = "J";
+var i = "I";
+var m = "M";
+var y = "Y";
+  function J() {
+      this.xpos = side2.random(w/2, w/2);
+      this.ypos = side2.random(h/2, h/2);
+      if (w < 600) {
+        this.xspeed = side2.random(-0.7, 0.7);
+        this.yspeed = side2.random(-0.7, 0.7);
+      }
+      else if (w >= 600) {
+        this.xspeed = side2.random(-2.0, 2.0);
+        this.yspeed = side2.random(-2.0, 2.0);
+      }
+    
+    this.display = function() {
+      side2.text(j, this.xpos, this.ypos);
+    }
+    this.move = function() {
+      this.xpos = this.xpos + this.xspeed;
+    if (this.xpos  <= w/6 || this.xpos >= w-(w/6)) {
+        this.xspeed = this.xspeed * -1;
+      }
+      this.ypos = this.ypos + this.yspeed;
+    if (this.ypos <= h/7 || this.ypos >= h-(h/5)) {
+        this.yspeed = this.yspeed * -1;
+      }
+      this.ypos = side2.constrain(this.ypos, h/7, h-(h/5));
+      this.xpos = side2.constrain(this.xpos, w/6, w-(w/6));
+    }
+  }
+
+  function I() {
+      this.xpos = side2.random(w/2, w/2);
+      this.ypos = side2.random(h/2, h/2);
+      if (w < 600) {
+        this.xspeed = side2.random(-0.7, 0.7);
+        this.yspeed = side2.random(-0.7, 0.7);
+      }
+      else if (w >= 600) {
+        this.xspeed = side2.random(-2.0, 2.0);
+        this.yspeed = side2.random(-2.0, 2.0);
+      }
+    
+    this.display = function() {
+      side2.text(i, this.xpos, this.ypos);
+    }
+    this.move = function() {
+      this.xpos = this.xpos + this.xspeed;
+    if (this.xpos  <= w/6 || this.xpos >= w-(w/6)) {
+        this.xspeed = this.xspeed * -1;
+      }
+      this.ypos = this.ypos + this.yspeed;
+    if (this.ypos <= h/7 || this.ypos >= h-(h/5)) {
+        this.yspeed = this.yspeed * -1;
+      }
+      this.ypos = side2.constrain(this.ypos, h/7, h-(h/5));
+      this.xpos = side2.constrain(this.xpos, w/6, w-(w/6));
+    }
+  }
+
+  function M() {
+      this.xpos = side2.random(w/2, w/2);
+      this.ypos = side2.random(h/2, h/2);
+      if (w < 600) {
+        this.xspeed = side2.random(-0.7, 0.7);
+        this.yspeed = side2.random(-0.7, 0.7);
+      }
+      else if (w >= 600) {
+        this.xspeed = side2.random(-2.0, 2.0);
+        this.yspeed = side2.random(-2.0, 2.0);
+      }
+    
+    this.display = function() {
+      side2.text(m, this.xpos, this.ypos);
+    }
+    this.move = function() {
+      this.xpos = this.xpos + this.xspeed;
+    if (this.xpos  <= w/6 || this.xpos >= w-(w/6)) {
+        this.xspeed = this.xspeed * -1;
+      }
+      this.ypos = this.ypos + this.yspeed;
+    if (this.ypos <= h/7 || this.ypos >= h-(h/5)) {
+        this.yspeed = this.yspeed * -1;
+      }
+      this.ypos = side2.constrain(this.ypos, h/7, h-(h/5));
+      this.xpos = side2.constrain(this.xpos, w/6, w-(w/6));
+    }
+  }
+
+  function Y() {
+      this.xpos = side2.random(w/2, w/2);
+      this.ypos = side2.random(h/2, h/2);
+      if (w < 600) {
+        this.xspeed = side2.random(-0.7, 0.7);
+        this.yspeed = side2.random(-0.7, 0.7);
+      }
+      else if (w >= 600) {
+        this.xspeed = side2.random(-2.0, 2.0);
+        this.yspeed = side2.random(-2.0, 2.0);
+      }
+    
+    this.display = function() {
+      side2.text(y, this.xpos, this.ypos);
+    }
+    this.move = function() {
+      this.xpos = this.xpos + this.xspeed;
+    if (this.xpos  <= w/6 || this.xpos >= w-(w/6)) {
+        this.xspeed = this.xspeed * -1;
+      }
+      this.ypos = this.ypos + this.yspeed;
+    if (this.ypos <= h/7 || this.ypos >= h-(h/5)) {
+        this.yspeed = this.yspeed * -1;
+      }
+      this.ypos = side2.constrain(this.ypos, h/7, h-(h/5));
+      this.xpos = side2.constrain(this.xpos, w/6, w-(w/6));
+    }
+  }
+
+
+  side2.preload = function() {
+    font = side2.loadFont("/portfolio/logo/assets/font.otf");
+  }
+
+  side2.setup = function() {
+    var side2parent = side2.createCanvas(300,300);
+    side2parent.parent("left");
+    side2.textFont(font);
+    side2.textSize(w/3);
+    side2.fill(0);
+    side2.textAlign(side2.CENTER,side2.CENTER);
+    letterj = new J();
+    letteri = new I();
+    letterm1 = new M();
+    letterm2 = new M();
+    lettery = new Y();
+  }
+
+  side2.draw = function() {
+    side2.background(255);
+    letterj.display();
+    letterj.move();
+    letteri.display();
+    letteri.move();
+    letterm1.display();
+    letterm1.move();
+    letterm2.display();
+    letterm2.move();
+    lettery.display();
+    lettery.move();
+    
+  }
+}
+var myp5 = new p5(side2);
