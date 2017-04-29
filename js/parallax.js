08addbe86ee2c0c00e5479a2e82a0920185724fa
JimmyTheDon <jdon@cca.edu>
2017-04-29 12:25:45 -0700

    Ugh

--- js/parallax.js
+++ js/parallax.js
@@ -2,10 +2,5 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		parallax: true,
 		parallaxKey: 'amltbXlkb24ubmV0X004R2NHRnlZV3hzWVhnPXBTNw==',
-		$('#checkbox').change(function(){
-			setInterval(function () {
-				moveRight();
-			}, 3000);
-		});
 	});
 });
